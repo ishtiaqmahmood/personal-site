829090d7a843fe9a73fcb2bddd64851bd874b5b8
ishtiaqmahmood <ishtiaqmahmood45@gmail.com>
2023-01-06 18:13:52 +0600

    version 1.0.0

---
+++ src/content_option.js
@@ -0,0 +1,216 @@
+const logotext = 'Ishtiaq Mahmood';
+const meta = {
+  title: 'Ishtiaq Mahmood',
+  description:
+    'I’m Ishtiaq Mahmood data scientist _ Full stack devloper,currently working in PicoCoders',
+};
+
+const introdata = {
+  title: 'I’m Ishtiaq Mahmood',
+  animated: {
+    first: 'I love coding',
+    second: 'I code cool websites',
+    third: 'I develop mobile apps',
+  },
+  description: 'Food , Code and Guitar',
+  your_img_url: './me.jpeg',
+};
+
+const dataabout = {
+  title: 'A bit about my self',
+  aboutme:
+    'I am Ishtiaq Mahmood, a programmer with a wealth of experience in MERN, Wordpress, React Native, Python, Data Analysis and Visualization. I have dedicated myself to continually learning and expanding my skillset through various online courses from professional platforms. My hard work and dedication to my craft have led me to co-founder and serve as the CTO of PicoCoders, a successful tech agency. My journey in technology began with a degree in Physics, but it was through coding that I found my true passion. I am excited to bring my skills and expertise to any team and make a positive impact.',
+};
+const worktimeline = [
+  {
+    jobtitle: 'Quantic Dynamics',
+    where: 'Dhaka',
+    date: '2020',
+  },
+  {
+    jobtitle: 'Cryptotech',
+    where: 'Sylhet',
+    date: '2021',
+  },
+  {
+    jobtitle: 'Picocoders',
+    where: 'Sylhet',
+    date: '2023',
+  },
+];
+
+const skills = [
+  {
+    name: 'Python',
+    value: 90,
+  },
+  {
+    name: 'PHP',
+    value: 70,
+  },
+  {
+    name: 'Javascript',
+    value: 90,
+  },
+  {
+    name: 'MERN',
+    value: 85,
+  },
+  {
+    name: 'React',
+    value: 85,
+  },
+  {
+    name: 'React Native',
+    value: 85,
+  },
+  {
+    name: 'Machine Learning',
+    value: 75,
+  },
+  {
+    name: 'Data Analysis and Visualization',
+    value: 88,
+  },
+  {
+    name: 'Solidity',
+    value: 80,
+  },
+  {
+    name: 'Blockchain Development',
+    value: 80,
+  },
+  {
+    name: 'Wordpress',
+    value: 90,
+  },
+  {
+    name: 'Git',
+    value: 95,
+  },
+];
+
+const services = [
+  {
+    title: 'Web Developer',
+    description: 'MERN - React - Blockchain Development - Wordpress',
+  },
+  {
+    title: 'Mobile Apps',
+    description: 'React Native',
+  },
+  {
+    title: 'Data Science',
+    description:
+      'Data Mining - Web Scraping - Data Analysis - Data Visualization - Mechine Learning',
+  },
+];
+
+const dataportfolio = [
+  {
+    img: 'https://picsum.photos/400/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/800/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/600/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/300/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/700/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+
+  {
+    img: 'https://picsum.photos/400/600/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/300/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/550/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://picsum.photos/400/700/?grayscale',
+    desctiption:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: 'ishtiaqmahmood22@gmail.com',
+  YOUR_FONE: '+8801684218109',
+  description:
+    'Feel free to contact me anytime If you have any questions. Thanks for your time.',
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: 'service_7kc94pa',
+  YOUR_TEMPLATE_ID: 'template_wt242bm',
+  YOUR_USER_ID: 'user_VzRjdJ9y6zeHlGWouNKVU',
+};
+
+const socialprofils = {
+  github: 'https://github.com/ishtiaqmahmood',
+  facebook: 'https://www.facebook.com/ishtiaq.mahmood.37',
+  linkedin: 'https://www.linkedin.com/in/ishtiaq-mahmood-470a741b9',
+  twitter: 'https://twitter.com/gatlinghutch001',
+  youtube: 'https://www.youtube.com/@ishtiaqmahmood',
+  twitch: 'https://twitch.com',
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
